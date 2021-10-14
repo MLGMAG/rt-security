b36711218d656ae366dd13a07b721970e5ad8384
Andrii Makrushyn <a.makrushyn@uaiev8503nbl.levi9.com>
2021-10-14 11:15:04 +0300

    [Project] Initial commit

---
+++ src/main/java/com/webmuffins/rtsx/security/RtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.webmuffins.rtsx.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webmuffins/rtsx/security/RtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmuffins.rtsx.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
