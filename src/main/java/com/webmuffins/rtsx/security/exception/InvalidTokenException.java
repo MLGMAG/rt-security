5f28a81eaa496f0f943cbddf76a1edfdcd7dd893
Andrii Makrushyn <a.makrushyn@uaiev8503nbl.levi9.com>
2021-10-15 21:08:24 +0300

    remove redundant import

--- src/main/java/com/webmuffins/rtsx/security/exception/InvalidTokenException.java
+++ src/main/java/com/webmuffins/rtsx/security/exception/InvalidTokenException.java
@@ -1,7 +1,5 @@
 package com.webmuffins.rtsx.security.exception;
 
-import org.springframework.security.core.AuthenticationException;
-
 public class InvalidTokenException extends RuntimeException {
 
     public InvalidTokenException(String message) {
